ab04be60d864f9a04f9b3d5271bcc61c4eea780f
Daffa Finanda <daffafinanda15@gmail.com>
2024-12-28 14:56:34 +0700

    error modal

--- src/components/errorModal.tsx
+++ src/components/errorModal.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { MdErrorOutline } from "react-icons/md";
 
 interface ErrorModalProps {
   message: string;
@@ -16,7 +17,8 @@ const ErrorModal: React.FC<ErrorModalProps> = ({ message, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
+      <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col">
+      <MdErrorOutline />
         <h2 className="text-xl font-bold mb-4 text-red-600">Error</h2>
         <p className="text-gray-700">{message}</p>
       </div>
